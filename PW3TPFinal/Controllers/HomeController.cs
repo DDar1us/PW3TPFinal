77dc30d3d380a61db87675927e4ba1bbb4b3478d
José <darius05g4@hotmail.com>
2017-06-10 22:49:58 -0300

    Migración parcial, ir corrigiendo lo resaltado
    Migración parcial, ir corrigiendo lo resaltado

---
+++ PW3TPFinal/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using PW3TPFinal.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PW3TPFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Usuarios u)
+        {
+            return RedirectToAction("Inicio");
+        }
+    }
+}
---
+++ PW3TPFinal/Controllers/PeliculasController.cs
@@ -0,0 +1,28 @@
+﻿using PW3TPFinal.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PW3TPFinal.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Reserva()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Peliculas p)
+        {
+            return RedirectToAction("Inicio");
+        }
+    }
+}
